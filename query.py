6bb3149a7d055d4259ff56c4f2e75f920decf623
Dev Masrani <dsmasrani@gmail.com>
2023-08-06 11:29:21 -0700

    Updated Secret

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-embeddings = OpenAIEmbeddings(openai_api_key='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')
+embeddings = OpenAIEmbeddings(openai_api_key=)
 docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name="studybuddy")
