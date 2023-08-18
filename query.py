a8a37842b51c43f1976c8cacf1d71d8c0afdaaee
Dev Masrani <dsmasrani@gmail.com>
2023-08-18 09:16:54 -0700

    Updated System to be better split up and supporting pair programming

--- query.py
+++
@@ -1,4 +0,0 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-
-embeddings = OpenAIEmbeddings(openai_api_key=)
-docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name="studybuddy")
