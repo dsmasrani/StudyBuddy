ce00f8ae9f17b8ea601b1a950a300aac4af1ec3a
Rotih <rohitbathula2020@gmail.com>
2023-12-05 16:23:59 -0800

    oh

--- backend/api/src/api/ingestion.py
+++ backend/api/src/api/ingestion.py
@@ -16,11 +16,13 @@ router = APIRouter(
 def initialize_embeddings(file_id: int, user_id: str):
     """"""
     with db.engine.begin() as connection:
-        user_keys_query = sqlalchemy.text("SELECT * FROM user_keys WHERE id = :user_id")
+        user_keys_query = sqlalchemy.text("SELECT * FROM user_keys WHERE user_id = :user_id")
         file_info_query = sqlalchemy.text("SELECT * FROM files WHERE id = :file_id")
 
         user_keys_result = connection.execute(user_keys_query, {'user_id': user_id}).fetchone()
         file_info_result = connection.execute(file_info_query, {'file_id': file_id}).fetchone()
+        print(user_keys_result)
+        print(file_info_result)
 
         if not user_keys_result or not file_info_result:
             return "User keys or file not found"
