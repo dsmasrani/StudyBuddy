a3de88a1f1ff685500e519ddc724ba5aa5158b49
Rotih <rohitbathula2020@gmail.com>
2024-01-04 18:34:18 -0800

    files

--- api/src/api/ingestion.py
+++ api/src/api/ingestion.py
@@ -6,7 +6,12 @@ from api.src import database as db
 import httpx
 import os
 import dotenv
+import os
+from supabase import create_client, Client
 
+url: str = os.environ.get("PROJECT_URL")
+key: str = os.environ.get("PROJECT_KEY")
+supabase: Client = create_client(url, key)
 
 router = APIRouter(
     prefix="/ingestion",
@@ -16,34 +21,10 @@ router = APIRouter(
 
 
 @router.get("/retrieve_files")
-async def retrieve_embeddings():
+def retrieve_embeddings():
     """"""
-    dotenv.load_dotenv()
-    key=str(os.environ.get("PROJECT_KEY"))
-    url=str(os.environ.get("PROJECT_URL"))
-    print(key)
-    print(url)
-    print(f"Bearer {key}")
-    curl = f"{url}/storage/v1/object/list/files"
-    print(curl)
-    headers = {
-        "authorization": f"Bearer {key}"
-    }
-    data = {
-        "prefix": "", 
-        "limit": 100  
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(curl, json=data, headers=headers)
-
-    #if response.status_code != 200:
-    #   raise HTTPException(status_code=response.status_code, detail="Error listing bucket files")
-
-    file_list = response.json()
-    print(file_list)
-
-    return {"message": "Ingestion initialized successfully"}
+    res = supabase.storage.from_('files').list()
+    return res
     
     
 @router.post("/upload_embeddings")
